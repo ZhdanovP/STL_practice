2933c5ff9b66a1e9af1a007eff8c386ce78b339e
Oleksii Cherkashyn <caffeine74@gmail.com>
2021-09-08 16:11:49 +0000

    Cherkashyn

--- include/impl.h
+++ include/impl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "bar_serving.h"
 #include <functional>
+#include <map>
 
 /**
  * @todo Implement functor-generator that will return next beer brand (cyclic)
@@ -8,7 +9,14 @@
  */
 struct BeerOrganizer
 {
+    int i = 0;
+    const std::array<BeerBrand, 6> recipe = { BeerBrand::HoeGaarden, BeerBrand::Corona, BeerBrand::Carlsberg, BeerBrand::Bud, BeerBrand::ZlataPraha, BeerBrand::Leffe};
+    const BeerBrand& operator()()
+    {
+        return recipe[ (i++)%recipe.size() ];
+    }
 };
+
 
 /**
  * @todo Implement unary predicate function that will determine if beer is expensive
@@ -16,20 +24,24 @@ struct BeerOrganizer
  *
  * @note Only Corona and HoeGaarden are expensive
  */
-bool isExpensiveBeer(/**???*/)
+bool isExpensiveBeer( BeerBrand beer )
 {
+    return ( beer == BeerBrand::HoeGaarden || beer == BeerBrand::Corona );
 }
 
 /**
  * @todo Implement lambda beer country equality comparator
  * @return true if beer county is the same, false otherwise
  */
-auto sameCountry = [](/**???*/)
+auto sameCountry = []( BeerBrand first, BeerBrand second )
 {
+    return  getBeerCountry( first ) == getBeerCountry( second );
 };
 
 struct MixingPolicy
 {
+    static const std::map<std::pair< AlcoholDrink, NonAlcoholDrink>, Cocktail > recipe;
+    
     /**
      * @todo Implement static transformator function
      * @return crafted cocktail
@@ -40,9 +52,17 @@ struct MixingPolicy
      * Whiskey + SevenUp = SevenPlusSeven;
      * Others + Others = Oops;
      */
-    static Cocktail mix(/**???*/)
+    static Cocktail mix( const AlcoholDrink & first, const NonAlcoholDrink & second )
     {
+        try
+        {
+            return recipe.at( std::make_pair(first,second) );
+        }
+        catch(...)
+        {
+            return Cocktail::Oops;
+        }
     }
 };
 
-std::function</**???*/> mixer {&MixingPolicy::mix};
+std::function<Cocktail(const AlcoholDrink &, const NonAlcoholDrink &)> mixer {&MixingPolicy::mix};
--- src/impl.cpp
+++ src/impl.cpp
@@ -1,2 +1,15 @@
 #include "impl.h"
 
+
+    /**
+     * @note Cookbook:
+     * Gin + Lime juice = Gimlet;
+     * Gin + Grapefruit juice = Greyhount;
+     * Whiskey + SevenUp = SevenPlusSeven;
+     * Others + Others = Oops;
+     */
+const std::map<std::pair< AlcoholDrink, NonAlcoholDrink>, Cocktail > MixingPolicy::recipe = {
+    {{ AlcoholDrink::Gin,     NonAlcoholDrink::LimeJuice},          Cocktail::Gimlet},
+    {{ AlcoholDrink::Gin,     NonAlcoholDrink::GrapefruitJuice},    Cocktail::Greyhount},
+    {{ AlcoholDrink::Whiskey, NonAlcoholDrink::SevenUp},            Cocktail::SevenPlusSeven},
+};
