7f7ecd990506bbba9af2283f3a0ca06172bc3c6b
vnevzorov <vogon404@gmail.com>
2021-09-19 18:50:04 +0300

    sequence task 2: all tests passed

--- include/impl.h
+++ include/impl.h
@@ -4,14 +4,18 @@
 /** @todo implement effective list rotation algorithm
  *
  * T - value type
- * BI - iterator type
+ * BI - Bidirectional iterator type
  *
  * @param list - container, for which rotation will be done
  * @param begin - begin of sequence that will be rotated
- * @param end - end of a sequence that will be rotated
  * @param new_begin - new position of rotated sequence
+ * @param end - end of a sequence that will be rotated
  *
  * @note See https://en.cppreference.com/w/cpp/algorithm/rotate
  */
-template<class T, class BI>
-void rotate(std::list<T>& list, BI begin, BI new_begin, BI end);
+template <class T, class BI>
+void rotate( std::list<T>& list, BI begin, BI new_begin, BI end )
+{
+   // it means just insert before pos new_begin all stuff from [begin; end)
+   list.splice( new_begin, list, begin, end );
+}
