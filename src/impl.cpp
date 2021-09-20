0374fb1d88a83a2224523ea2b0c88ccfbb8ba20a
S-A-Sidorenko <56437479+S-A-Sidorenko@users.noreply.github.com>
2021-09-20 18:08:18 +0300

    task_sequence_3 Sidorenko Sergei

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,2 +1,30 @@
 #include "impl.h"
 
+std::string reverseWords(const std::string& input)
+{
+    if (input.empty()) {
+        return std::string();
+    }
+
+    std::stringstream stream(input);
+    std::deque<std::string> words;
+    while (stream) {
+        std::string word;
+        stream >> word;
+        if (!word.empty()) {
+            words.push_back(word);
+        }
+    }
+    if (words.empty()) {
+        return std::string();
+    }
+
+    std::string reversed = words.back();
+    words.pop_back();
+
+    std::for_each(words.begin(), words.end(), [&reversed](const std::string &eachWord){
+        reversed.push_back(' ');
+        std::copy(eachWord.begin(), eachWord.end(), std::back_inserter(reversed));
+
+    });
+}
