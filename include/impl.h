9535f2ff64e6918f45620a65fa10dce89ee7fccf
Oleksii Cherkashyn <caffeine74@gmail.com>
2021-09-21 14:47:07 +0000

    Task 3 (Cherkashyn)

--- include/impl.h
+++ include/impl.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <sstream>
-#include <deque>
 #include <iterator>
+#include <numeric>
+#include <forward_list>
+#include <iostream>
 
 /**
  * @todo Implement function that will reverse words in a sentence
@@ -11,4 +13,17 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+std::string reverseWords(const std::string& input)
+{
+    std::stringstream inputStream( input );
+    std::forward_list<std::string> words;
+    std::copy( std::istream_iterator<std::string>( inputStream ),
+               std::istream_iterator<std::string>(),
+               std::front_inserter( words ) );
+
+    return std::accumulate( words.begin(), words.end(), std::string(""),
+                        []( const std::string& a, const std::string& b )
+                        {
+                            return ( a.empty() ? b : a + " " + b );
+                        });
+}
