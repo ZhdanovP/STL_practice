4125a36611d967c83d51a4cd916372f3d7892de9
vnevzorov <vogon404@gmail.com>
2021-09-22 12:33:12 +0300

    convert grade v2

--- src/impl.cpp
+++ src/impl.cpp
@@ -2,18 +2,10 @@
 #include <cstdint> // uint8_t
 #include <map>
 
-static const std::map<std::pair<uint8_t, uint8_t>, uint8_t> gradesFrom100to5{
-    { { 0, 29 }, 1 }, { { 30, 49 }, 2 }, { { 50, 69 }, 3 }, { { 70, 89 }, 4 }, { { 90, 100 }, 5 } };
+static const std::map<uint8_t, uint8_t> gradesFrom100to5{
+    { 29, 1 }, { 49, 2 }, { 69, 3 }, { 89, 4 }, { 255, 5 } };
 
 uint8_t convertTo5PointGrade( uint8_t grade100Point )
 {
-   for ( const auto& [range, grade5Point] : gradesFrom100to5 )
-   {
-      if ( range.first <= grade100Point && grade100Point <= range.second )
-      {
-         return grade5Point;
-      }
-   }
-
-   return ( *gradesFrom100to5.rbegin() ).second;
+   return ( *gradesFrom100to5.lower_bound( grade100Point ) ).second;
 }
