a6543f17e3372f75fca9caa1741d5c33ebd4e6db
ZhdanovP <pavel.zhdanov@neuralegion.com>
2021-09-22 18:39:39 +0300

    Add src associative 1

--- include/impl.h
+++ include/impl.h
@@ -2,9 +2,14 @@
 #include <string>
 
 /**
- You must implement a conversion from 100-point grade system to 5-point system.
-
- Example: 0-29 – 1;  30-49 – 2;
-        50-69 – 3; 70-89 – 4;
-        90+ - 5;
+ * @todo Implement function that will check if one string has the same characters
+ * as the second one
+ *
+ * @example is_anagram("abcd", "acdb") -> true
+ * @example is_anagram("abcd", "abec") -> false
+ *
+ * @param first - first word
+ * @param second - second word
+ * @return true if first is anagram of second
  */
+bool is_anagram(const std::string& first, const std::string& second);
