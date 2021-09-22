57f6dd95477c721b47660f85d818200d45d33282
Oleksandr Chernukha <Ochernukha@luxoft.com>
2021-09-22 19:01:01 +0300

    Aleksandr Chernukha: task_adapters_2

--- tests/unit_tests/src/maintest.cpp
+++ tests/unit_tests/src/maintest.cpp
@@ -1,8 +1,35 @@
 #include "maintest.h"
+#include "impl.h"
 
 using namespace ::testing;
 
-TEST(Convertor, Simple)
+/**
+ You must implement a conversion from 100-point grade system to 5-point system.
+
+ Example: 0-29 – 1;  30-49 – 2;
+        50-69 – 3; 70-89 – 4;
+        90+ - 5;
+ */
+
+TEST(Convertor, convertGrade)
 {
-    EXPECT_TRUE(true);
+    EXPECT_EQ(convertGrade(0), 1);
+    EXPECT_EQ(convertGrade(29), 1);
+
+    EXPECT_EQ(convertGrade(30), 2);
+    EXPECT_EQ(convertGrade(49), 2);
+
+    EXPECT_EQ(convertGrade(50), 3);
+    EXPECT_EQ(convertGrade(69), 3);
+
+    EXPECT_EQ(convertGrade(70), 4);
+    EXPECT_EQ(convertGrade(89), 4);
+
+    EXPECT_EQ(convertGrade(90), 5);
+    EXPECT_EQ(convertGrade(91), 5);
+    EXPECT_EQ(convertGrade(99), 5);
+
+    EXPECT_EQ(convertGrade(100), 5);
+    EXPECT_EQ(convertGrade(200), 5);
+    EXPECT_EQ(convertGrade(300), 5);
 }
