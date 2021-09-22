8d4c8d876cd4cc2ad0d06b39083dfda0336006d9
vnevzorov <vogon404@gmail.com>
2021-09-22 13:38:30 +0300

    fix naming and add comments

--- src/impl.cpp
+++ src/impl.cpp
@@ -2,10 +2,16 @@
 #include <cstdint> // uint8_t
 #include <map>
 
-static const std::map<uint8_t, uint8_t> gradesFrom100to5{
+/*
+ * Table for convertion: 100 point -> 5 point
+ * 100 point grades values set as upper range values for each grade range:
+ * [0;29], [30;49], [50;69], [70;89], [90;MAX]
+ *   1        2        3        4        5
+ */
+static const std::map<uint8_t, uint8_t> tableBounds100to5{
     { 29, 1 }, { 49, 2 }, { 69, 3 }, { 89, 4 }, { 255, 5 } };
 
 uint8_t convertTo5PointGrade( uint8_t grade100Point )
 {
-   return ( *gradesFrom100to5.lower_bound( grade100Point ) ).second;
+   return ( *tableBounds100to5.lower_bound( grade100Point ) ).second;
 }
