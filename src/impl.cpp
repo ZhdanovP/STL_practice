223a4d91d6c983c791d5e2393d61fe0123aeff88
S-A-Sidorenko <56437479+S-A-Sidorenko@users.noreply.github.com>
2021-09-22 19:00:48 +0300

    Add files via upload

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,35 @@
 #include "impl.h"
+#include <map>
+
+std::map<unsigned int, unsigned int> createConvertor()
+{
+    std::map<unsigned int, unsigned int> convertor;
+    for (unsigned int i = 0; i < 30; ++i) {
+        convertor[i] = 1;
+    }
+    for (unsigned int i = 30; i < 50; ++i) {
+        convertor[i] = 2;
+    }
+    for (unsigned int i = 50; i < 70; ++i) {
+        convertor[i] = 3;
+    }
+    for (unsigned int i = 70; i < 90; ++i) {
+        convertor[i] = 4;
+    }
+    for (unsigned int i = 90; i < 101; ++i) {
+        convertor[i] = 5;
+    }
+    return convertor;
+}
+
+const std::map<unsigned int, unsigned int> convertor = createConvertor();
+
+std::pair<bool, unsigned int> convert(const unsigned int &mark)
+{
+    auto it = convertor.find(mark);
+    if (it == convertor.end()) {
+        return std::make_pair(false, 0);
+    }
+
+    return std::make_pair(true, it->second);
+}
