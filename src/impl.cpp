d718798f7c736b348e3e50d0a8136808e2a2ca97
S-A-Sidorenko <56437479+S-A-Sidorenko@users.noreply.github.com>
2021-09-23 18:19:19 +0300

    task_adaptors_1_Sidorenko_Sergei

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,8 +1,35 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
+#include <algorithm>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    std::stack<std::string::value_type> bracketsInText;
+    for (const auto& ch : source) {
+        auto it = std::find_if(brackets.begin(), brackets.end(), [&ch](const std::pair<char, char> &pair) {
+            return ch == pair.first || ch == pair.second;
+        });
+        if (it == brackets.end()) {
+            continue; // skip
+        }
+
+        if (ch == it->first) {
+            bracketsInText.push(ch);
+        } else { // ch == it->second
+            if (it->first == bracketsInText.top()) {
+                bracketsInText.pop();
+            } else {
+                break;
+            }
+        }
+    }
+
+    return bracketsInText.empty();
+}
