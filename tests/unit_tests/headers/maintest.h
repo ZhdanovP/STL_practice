4cc025ffc13dda063c753b4f25c24de33762d420
ZhdanovP <pavel.zhdanov@neuralegion.com>
2021-09-23 19:39:19 +0300

    Add header for utests

--- tests/unit_tests/headers/maintest.h
+++ tests/unit_tests/headers/maintest.h
@@ -1,43 +1,73 @@
 #pragma once
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include <vector>
-#define private public // <- communism
 #include "impl.h"
+#include <experimental/filesystem>
+#include <fstream>
 
-class Regression : public ::testing::Test
+namespace fs = std::experimental::filesystem;
+
+template<int Files>
+class NFiles : public ::testing::Test
 {
-public:
+protected:
+    std::array<std::ofstream, Files> files;
     void SetUp() override
     {
-        m_browser.userEnter("1");
-        m_browser.userEnter("2");
-        m_browser.userEnter("3");
-        m_browser.userEnter("4");
-        m_browser.userEnter("5");
+        std::error_code success;
+        fs::create_directory("sandbox", success);
+        for (size_t i = 0; i < files.size(); i++)
+        {
+            files.at(i).open("sandbox/file" + std::to_string(i) + ".txt");
+        }
     }
+
+    void TearDown() override
+    {
+        std::error_code success;
+        for (size_t i = 0; i < files.size(); i++)
+        {
+            fs::remove("sandbox/file" + std::to_string(i) + ".txt");
+        }
+        fs::remove("sandbox", success);
+    }
+};
+
+class ThreeFiles : public NFiles<3>
+{
+};
+
+class NineFiles : public NFiles<9>
+{
+};
+
+class TwoFolders : public ::testing::Test
+{
 protected:
-    DataBrowser m_browser;
+    std::array<std::ofstream, 4> files;
+    void SetUp() override
+    {
+        std::error_code success;
+        fs::create_directory("sandbox", success);
+        fs::create_directory("sandbox/sandbox1", success);
+        fs::create_directory("sandbox/sandbox2", success);
+        for (size_t i = 0; i < files.size(); i++)
+        {
+            files.at(i).open("sandbox/sandbox" + std::to_string((i % 2) + 1) +"/file" + std::to_string(i) + ".txt");
+        }
+    }
+
+    void TearDown() override
+    {
+        std::error_code success;
+        for (size_t i = 0; i < files.size(); i++)
+        {
+            fs::remove("sandbox/sandbox" + std::to_string((i % 2) + 1) + "/file" + std::to_string(i) + ".txt");
+
+        }
+        fs::remove("sandbox/sandbox1", success);
+        fs::remove("sandbox/sandbox2", success);
+        fs::remove("sandbox", success);
+    }
 };
 
-class SafeCall : public Regression
-{
-};
-
-class InvokeDataRequestCall : public Regression
-{
-};
-
-class MockDataSelector : public IDataSelector
-{
-public:
-    MOCK_METHOD(bool, getDataType1,
-                (std::vector<size_t>& returnValues, const size_t unusedDataShouldBeZero),
-                (override, const));
-    MOCK_METHOD(bool, getDataType2,
-                (std::vector<size_t>& returnValues),
-                (override, const));
-    MOCK_METHOD(bool, getDataType3,
-                (std::deque<size_t>& returnValues),
-                (override, const));
-};
