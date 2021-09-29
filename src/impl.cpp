e0d4ea74e75fbe46ee2bcc23dfd3c0c3d60309da
alex1968 <aljp@ukr.net>
2021-09-29 14:21:01 +0300

    Aleksandr Zakharov task_assoative_az

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,21 @@
 #include "impl.h"
+#include <set>
+#include <algorithm>
+#include <iterator>
+
+bool is_anagram(const std::string& first, const std::string& second)
+{
+   std::set<char> set_first;
+   std::for_each(first.begin(), first.end(), [&set_first](const char & ch)
+      {
+         set_first.insert(ch);
+      });
+
+   std::set<char> set_second;
+   std::for_each(second.begin(), second.end(), [&set_second](const char& ch)
+      {
+         set_second.insert(ch);
+      });
+
+   return set_first == set_second && first.size() == second.size();
+}
